d94d0fa9a513397389ac6e8147162c06fab99241
Fran Escriche Grau <franchupinazo@hotmail.es>
2018-10-18 18:19:55 +0200

    asd

---
+++ Assets/Scripts/Navegation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navegation : MonoBehaviour {
+
+    private Vector2Int thisLastSquarePosition;
+    private Vector2Int targetLastSquarePosition;
+    public float angularSpeed = 120f;
+    public float speed = 3.5f;
+    public float acceleration = 8f;
+
+    public void SetDestination(Vector3 pos)
+    {
+
+    }
+
+}
