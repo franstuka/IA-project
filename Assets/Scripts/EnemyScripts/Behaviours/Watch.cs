5b3b92e456a0083cc7f150911cde25e37267d515
Jaume Sanz Sempere <al315564@uji.es>
2018-10-30 18:23:17 +0100

    asd

--- Assets/Scripts/EnemyScripts/Behaviours/Watch.cs
+++ Assets/Scripts/EnemyScripts/Behaviours/Watch.cs
@@ -37,26 +37,27 @@ public class Watch : BehaviourBase
         Vector3 dirToTarget = (target.position - raycastPos.position).normalized;
         float disToTarget = Vector3.Distance(raycastPos.position, target.position);
 
-            Debug.DrawRay(raycastPos.position, raycastPos.forward * watchRange, Color.red);
+        Debug.DrawRay(raycastPos.position, raycastPos.forward * watchRange, Color.red);
 
-            Vector3 vectorA = Quaternion.AngleAxis(viewAngle / 2, Vector3.up) * (raycastPos.forward * watchRange);
-            Vector3 vectorB = Quaternion.AngleAxis(-viewAngle / 2, Vector3.up) * (raycastPos.forward * watchRange);
+        Vector3 vectorA = Quaternion.AngleAxis(viewAngle, Vector3.up) * (raycastPos.forward * watchRange);
+        Vector3 vectorB = Quaternion.AngleAxis(-viewAngle, Vector3.up) * (raycastPos.forward * watchRange);
 
-            Debug.DrawRay(raycastPos.position, vectorA, Color.blue);
-            Debug.DrawRay(raycastPos.position, vectorB, Color.blue);
-            Debug.DrawLine(raycastPos.position, target.position, Color.green);
+        Debug.DrawRay(raycastPos.position, vectorA, Color.blue);
+        Debug.DrawRay(raycastPos.position, vectorB, Color.blue);
+        Debug.DrawLine(raycastPos.position, target.position, Color.green);
 
-            if (Vector3.Angle(raycastPos.forward, dirToTarget) < viewAngle / 2){
+        if (Vector3.Angle(raycastPos.forward, dirToTarget) < viewAngle){
 
-                if(!Physics.Raycast(raycastPos.position, dirToTarget, disToTarget, obstacleMask))
-                {
-                    return true;
-                }
-            }
-            else if(player.GetComponent<PlayerCombat>().GetSoundlevel() / disToTarget > 7f)
+            if(!Physics.Raycast(raycastPos.position, dirToTarget, disToTarget, obstacleMask))
             {
-                    return true;
+                return true;
             }
+        }
+        else if(player.GetComponent<PlayerCombat>().GetSoundlevel() / disToTarget > 7f)
+        {
+            Debug.Log("NO ESTOY SORDO");
+                return true;
+        }
 
         return false;
     }
